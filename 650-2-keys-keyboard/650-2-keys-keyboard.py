7c5b006a73b9d7741beb769a98a59b8c71ff8ae0
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-29 05:53:29 -0700

    Time: 1241 ms (14.07%), Space: 14.8 MB (28.24%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.py
+++ 650-2-keys-keyboard/650-2-keys-keyboard.py
@@ -2,7 +2,7 @@ class Solution:
     def minSteps(self, n: int) -> int:
         if n == 1:
             return 0
-        @cache
+        # @cache
         def rec(s,c):
             if s == n:
                 return 1
