93ab5965104b2e41096844d9eec22d3cfcfff92d
Endalebob <95867374+Endalebob@users.noreply.github.com>
2022-08-31 11:33:31 +0300

    Time: 241 ms (11.16%), Space: 17.3 MB (33.27%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,24 +1,34 @@
 class Solution:
     def findOrder(self, c: int, pre: List[List[int]]) -> List[int]:
         indegree = [0]*c
+        ans = []
+        gl_vstd = set()
         graph = defaultdict(list)
         for i,j in pre:
             graph[j].append(i)
             indegree[i]+=1
-        deq = deque()
+        root = []
         for i in range(c):
             if indegree[i] == 0:
-                deq.append(i)
-        ans = []
-        print(indegree)
-        while deq:
-            for i in range(len(deq)):
-                j = deq.popleft()
-                ans.append(j)
-                for k in graph[j]:
-                    indegree[k] -= 1
-                    if indegree[k] == 0:
-                        deq.append(k)
+                root.append(i)
+        def dfs(idx):
+            if idx in gl_vstd:
+                return True
+            if idx in vstd:
+                return False
+            vstd.add(idx)
+            for i in graph[idx]:
+                if not dfs(i):
+                    return False
+            vstd.remove(idx)
+            gl_vstd.add(idx)
+            ans.append(idx)
+            return True
+        for i in root:
+            vstd = set()
+            if not dfs(i):
+                return []
         if len(ans) == c:
-            return ans
+            return ans[::-1]
         return []
+            
